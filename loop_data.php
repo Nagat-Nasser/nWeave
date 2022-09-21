29e4756769a47d2b2ec892a75e9a09ef8ff8dc0c
Mayar Mohamed Ragab <110773490+Mayar55@users.noreply.github.com>
2022-09-21 20:38:22 +0200

    update project

---
+++ loop_data.php
@@ -0,0 +1,120 @@
+
+<div id="content12">
+    <div class="PA1">
+
+        <button class="button">
+            <!-- <a href=""><img src="./Assets/Imgs/Capture2.PNG" alt="imgside2"></a> -->
+        </button>
+
+        <label>
+            <input type="text" class="pagesearch" placeholder="Search...">
+        </label>
+        <button class="button">
+            <a href=""><img src="./Assets/Imgs/square_20220723_120015 (4).jpg" alt="imgpa0"></a>
+        </button>
+        <div class="pnentriespage">
+            <div class="pageent">Entries</div>
+            <input type="number" class="pagenum"></input>
+        </div>
+        <div class="filterpage">
+            <div class="pagefilter6">Filter By</div>
+            <input type="number" class="pagefilteby">
+        </div>
+        <input type="reset" class="pagereset"></input>
+        <span>
+            <button class="button">
+                <a href=""><img src="./Assets/Imgs/square_20220723_120015 (5).jpg" alt="imgpa1"></a>
+            </button>
+        </span>
+        <span>
+            <button class="button">
+                <a href=""><img src="./Assets/Imgs/square_20220723_120015 (1).jpg" alt="imgpa2"></a>
+            </button>
+        </span>
+        <span>
+            <button class="button">
+                <a href=""><img src="./Assets/Imgs/square_20220723_120015 (2).jpg" alt="imgpa3"></a>
+            </button>
+        </span>
+    </div>
+
+    <div class="pages">
+        <div class="formimg0">
+
+<!--            <button class="buttonform">-->
+<!--                <a href=""><img src="./Assets/Imgs/1.png" alt="imgform3"></a>-->
+<!--            </button>-->
+<!--            <button class="buttonform">-->
+<!--                <a href=""><img src="./Assets/Imgs/2.png" alt="imgform3"></a>-->
+<!--            </button>-->
+<!--            <button class="buttonform">-->
+<!--                <a href=""><img src="./Assets/Imgs/3.png" alt="imgform3"></a>-->
+<!--            </button>-->
+<!--            <h4>-->
+
+                <H5>Property Name</H5>
+            </h4>Address </h4>
+            <h4>City </h4>
+            <h4>Tenanst </h4>
+            <h4>Zip</h4>
+            <h4>Type</h4>
+            <h4>SubType</h4>
+            <h4>Size</h4>
+            <h4>GLA</h4>
+            <h4>Stake</h4>
+            </h4>
+
+        </div>
+        <div class="formimg1">
+
+                               <div class="icons">
+                        <button class="buttonform">
+                            <a href=""><img src="./Assets/Imgs/1.png" alt="imgform3"></a>
+                        </button>
+                        <button class="buttonform">
+                            <a href=""><img src="./Assets/Imgs/2.png" alt="imgform3"></a>
+                        </button>
+                        <button class="buttonform">
+                            <a href=""><img src="./Assets/Imgs/3.png" alt="imgform3"></a>
+                        </button>
+                        <h4>
+
+            </div>
+
+            <H5>Property Name</H5>
+            </h4>Address </h4>
+            <h4>City </h4>
+            <h4>Tenanst </h4>
+            <h4>Zip</h4>
+            <h4>Type</h4>
+            <h4>SubType</h4>
+            <h4>Size</h4>
+            <h4>GLA</h4>
+            <h4>Stake</h4>
+            </h4>
+
+        </div>
+    </div>
+
+
+    <div class="PA2">
+        <button class="PA2button">
+            <a href="#rightPA2"><img src="./Assets/Imgs/arrow-93-256.ico" alt="rightPA2" class="rightPA2"></a>
+        </button>
+        <button class="PA2button">
+            <a href="#pa2pre">
+                <h4 class="pa2pre">Previous</h4>
+            </a>
+        </button>
+        <h4 class="pa2pa">Page 1 Of 5</h4>
+        <button class="PA2button">
+            <a href="#pa2next">
+                <H4 class="pa2next">Next</H4>
+            </a>
+        </button>
+        <button class="PA2button">
+            <a href="#leftPA2"><img src="./Assets/Imgs/arrow-29-256.ico" alt="leftPA2" class="leftPA2"></a>
+        </button>
+    </div>
+</div>
+
