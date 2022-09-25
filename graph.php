6662188ee9c449251c1b42c075b04373eb25eb5b
Nagat_Nasser <nagatnasser989@gmail.com>
2022-09-25 13:37:53 +0200

    update graph file

--- graph.php
+++ graph.php
@@ -1,181 +1,67 @@
-
 <div id="content13">
     <div id="Graph_Header">
         <div class="lab1">From :</div>
         <input type="number" class="inp1">
         <div class="lab2">To :</div>
         <input type="number" class="inp2">
-        <input type="button" value="Filter" class="graph_buttonHeader">
-    </div>
-
-    <div id="length2">
-        <div class="divchart1">Vol ()</div>
-        <div class="divchart1">$ 30 b</div>
-        <div class="divchart1">$ 20 b</div>
-        <div class="divchart1">$ 10 b</div>
-        <div class="divchart1">$ 0.7 b </div>
-        <div class="divchart1">$ 0.2 b </div>
-        <div class="divchart1">$ 0 b </div>
-        <div class="divchart1">$ 0.2 b</div>
-        <div class="divchart1">$ 0.7 b</div>
-        <div class="divchart1">$ 10 b</div>
-        <div class="divchart1">$ 20 b</div>
-        <div class="divchart1">$ 30 b</div>
-
-    </div>
+        <input type="button" value="Filter" class="graph_buttonHeader"></div>
 
     <div id="charts2">
         <div class="scroll-chart">
             <div class="drawchart">
-                <div class="positive_part_of_graph">
-                    <?php
-                    for ($year_data = 2013; $year_data <= 2022; $year_data++) {?>
-                        <div class="Graph_Data_chart1">
-
+                <?php $grid_year_difference= $_GET['endYear']-$_GET['startYear']?>
+                <div class="a" style="grid-template-columns:repeat(<?php echo( $grid_year_difference + 1) ?>,1fr);">
+                        <?php $max=0;
+                        for ($year_data = $_GET['startYear']; $year_data <= $_GET['endYear']; $year_data++) {?>
+                            <div class="Graph_Data_chart1" id="<?php echo $year_data ?>">
                             <?php
-
-                            $Net_data = $graph_data->InvestmentActivityData->$year_data->NetValue;
-                            if ($Net_data != " 0.0" && $Net_data!=(-$Net_data)) {?>
-                                <?php
-                                if ($Net_data < 1000000) {
-                                    $format = number_format($Net_data);
-                                } else if ($Net_data < 1000000000) {
-                                    $format2 = number_format($Net_data / 1000000, 2);
-                                    echo '<div class="Net_data_chart" style="height:'?><?php echo ((int)$format2/(int)1000);?><?php echo '%">' . '</div>';
-
-                                } else {
-                                    $format = number_format($Net_data/ 1000000000, 2) ;
-                                    echo '<div class="Net_data_chart" style="height:'?><?php echo $format ;?><?php echo '%">' . '</div>';?>
-                                    <?php
-
-                                }}
-
-
-                            else {
-                                echo '<div class="Net_data_chart" style="display: none;"></div>';
-
-                            }
-
-
-
-
-
-                            $Acquisition_data = $graph_data->InvestmentActivityData->$year_data->TotalAcquisitionsPrice;
-                            if ($Acquisition_data != " 0.0") {?>
-                                <?php if ($Acquisition_data < 1000000) {
-                                    $format = number_format($Acquisition_data);
-                                } else if ($Acquisition_data < 1000000000) {
-                                    $format = number_format($Acquisition_data / 1000000, 2);
-                                    $format2 = number_format($Acquisition_data / 1000000, 2);
-                                    echo '$'.((int)$format2/(int)1000);
-                                } else {
-                                    $format = number_format($Acquisition_data/ 1000000000, 2);
-                                }
-                                echo '<div class="Acquisition_data_chart" style="height:'?><?php echo $format;?><?php echo '%">' . '</div>';?>
-                                <?php
-                            }else {
-                                echo '<div class="Acquisition_data_chart" style="display: none;"></div>';
-                            }
-                            ?>
-
-
-                        </div>
-                    <?php } ?>
-
-                </div>
-                <div class="negative_part_of_graph">
-                    <?php
-                    for ($year_data = 2013; $year_data <= 2022; $year_data++) {?>
-                        <div class="Graph_Data_chart2">
-                            <?php
-
-                            $Net_data = $graph_data->InvestmentActivityData->$year_data->NetValue;
-                            if ($Net_data != " 0.0" && $Net_data=(-$Net_data)) {
-                                if ($Net_data < 1000000) {
-                                    $format = number_format($Net_data);
-                                } else if ($Net_data < 1000000000) {
-                                    $format2 = number_format($Net_data / 1000000, 2);
-                                    echo '<div class="Net_data_chart" style="height:'?><?php echo ((int)$format2/(int)1000);?><?php echo '%">' . '</div>';
-                                } else {
-                                    $format = number_format($Net_data/ 1000000000, 2) ;
-                                    echo '<div class="Net_data_chart" style="height:'?><?php echo $format ;?><?php echo '%">' . '</div>';?>
-                                    <?php
-                                }}
-                            elseif($Net_data=$Net_data) {
-                                echo '<div class="Net_data_chart" style="display: none;"></div>';
-
-                            }
-
-                            else{
-                                echo '<div class="Net_data_chart" style="display: none;"></div>';
-                            }
-
-
-
-
-
-
-                            $Dispoaition_data = $graph_data->InvestmentActivityData->$year_data->TotalDispositionsPrice;
-                            if ($Dispoaition_data != " 0.0") {?>
-                                <?php
-                                if ($Dispoaition_data < 1000000) {
-                                    $format = number_format($Dispoaition_data);
-                                } else if ($Dispoaition_data < 1000000000) {
-                                    $format = number_format($Dispoaition_data / 1000000, 2) ;
-                                    $format2 = number_format($Dispoaition_data / 1000000, 2) ;
-                                    echo '$'.((int)$format2/(int)1000);
-                                } else {
-                                    $format = number_format($Dispoaition_data/ 1000000000, 2) ;
-                                }
-                                echo '<div class="Dispoaition_data_chart" style="height:'?><?php echo $format;?><?php echo '%">' . '</div>';?>
-
-                                <?php
-                            }else {
-                                echo '<div class="Dispoaition_data_chart" style="display: none;"></div>';
-                            }
-
-
-                            ?>
-                            <div class="width"><?php echo $year_data ?></div>
-
-                        </div>
-                    <?php  }?>
-
-
+                            $Acquisition_data = $graph_year_data->InvestmentActivityData->$year_data->TotalAcquisitionsPrice;
+                            $Net_data = $graph_year_data->InvestmentActivityData->$year_data->NetValue;
+                            foreach ($graph_year_data->InvestmentActivityData as $key => $value) {
+                            if($graph_year_data->InvestmentActivityData->{$key}->TotalAcquisitionsPrice>$max){
+                            $max=$graph_year_data->InvestmentActivityData->{$key}->TotalAcquisitionsPrice;}}
+                            //acquisition bar
+                            echo '<div class="Acquisition_data_chart" style="height:'?><?php echo number_format(($Acquisition_data /$max)*220,1);?><?php echo 'px">' . '</div>';
+                            //positive net bar
+                            if($Net_data>0) {
+                            echo '<div class="Net_data_chart" style="height:'?><?php echo abs(number_format(($Net_data / $max) * 220, 1)) ;?><?php echo 'px">' . '</div>';} ?>
+                            </div>
+                        <?php }?>
                 </div>
 
+                <div class="b" style="grid-template-columns:repeat(<?php echo( $grid_year_difference + 1) ?>,1fr);">
+                     <?php
+                        $max=0;
+                        for ($year_data = $_GET['startYear']; $year_data <= $_GET['endYear']; $year_data++) {?>
+                         <div class="Graph_Data_chart2" id="<?php echo $year_data ?>">
+                            <?php $Disposition_data = $graph_year_data->InvestmentActivityData->$year_data->TotalDispositionsPrice;
+                            $Net_data = $graph_year_data->InvestmentActivityData->$year_data->NetValue;
+                            foreach ($graph_year_data->InvestmentActivityData as $key => $value) {
+                                if($graph_year_data->InvestmentActivityData->{$key}->TotalAcquisitionsPrice>$max){
+                                $max=$graph_year_data->InvestmentActivityData->{$key}->TotalAcquisitionsPrice;}}
+                                //disposition bar
+                                echo '<div class="Disposition_data_chart" style="height:'?><?php echo number_format(($Disposition_data/$max)*220,1);?><?php echo 'px">' . '</div>';
+                                //negtivte net bar
+                                if($Net_data<0){
+                                echo '<div class="Net_data_chart" style="height:'?><?php echo abs(number_format(($Net_data / $max) * 220, 2));?><?php echo 'px">' . '</div>';} ?>
+                         </div>
+                       <?php }?>
+                    <!--                            <div class="width">--><?php //echo $year_data ?><!--</div>-->
 
-            </div>
-        </div>
-
-
-
-        <div class="Generalnote1">
-            <div class="note0">
-                <div class="  bubbybluechartn"></div>
-            </div>
-            <div class="note1">
-                <span class="Disposition">Disposition</span>
-            </div>
-            <div class="note0">
-                <div class="orangechartn"></div>
-            </div>
-            <div class="note1">
-                <span class="Acquisition">Acquisition</span>
-            </div>
-            <div class="note0">
-                <div class=" purblechartn"></div>
-            </div>
-            <div class="note1">
-                <span class="Net">Net</span>
+                </div>
             </div>
         </div>
     </div>
-</div>
-</div>
+    <div id="length2">
 
-
-
-
-
-
+        <?php $vol_counter=30;?>
+                   <div class="divchart1" style="height:<?php echo number_format(($max/7)/100000000,1)?>px">vol ()</div>
+<?php        for($loop_counter=0 ; $loop_counter<=6 ; $loop_counter++){?>
+            <div class="divchart1" style="height:<?php echo number_format(($max/7)/100000000,1)?>px"><?php  echo   $vol_counter .' B' ?> </div>
+             <?php $vol_counter -=5;
+             }?>
+           <?php $vol_counter=5;
+            for($loop_counter=0 ; $loop_counter<=4; $loop_counter++){?>
+            <div class="divchart1" style="height:<?php echo number_format(($max/7)/100000000,1)?>px"><?php  echo   $vol_counter .' B' ?> </div>
+            <?php $vol_counter +=5; }?>
+    </div>
