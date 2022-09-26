86701db14abb010a67d72da0eda3ba6e38b4d5a5
Nagat_Nasser <nagatnasser989@gmail.com>
2022-09-26 15:42:42 +0200

    update graph file

--- graph.php
+++ graph.php
@@ -4,15 +4,15 @@
         <input type="number" class="inp1">
         <div class="lab2">To :</div>
         <input type="number" class="inp2">
-        <input type="button" value="Filter" class="graph_buttonHeader"></div>
-    <div id="charts2">
+        <input type="button"  class="graph_buttonHeader"></div>
+    <div id="chart">
         <div class="scroll-chart">
             <div class="drawchart">
                 <?php $grid_year_difference= $_GET['endYear']-$_GET['startYear']?>
-                <div class="a" style="grid-template-columns:repeat(<?php echo( $grid_year_difference + 1) ?>,1fr);">
+                <div class="positive_part_of_graph" style="grid-template-columns:repeat(<?php echo( $grid_year_difference + 1) ?>,1fr);">
                     <?php
                     for ($year_data = $_GET['startYear']; $year_data <= $_GET['endYear']; $year_data++) {?>
-                        <div class="Graph_Data_chart1" id="<?php echo $year_data ?>">
+                        <div class="Graph_Data_positive_chart" id="<?php echo $year_data ?>">
                        <?php
                             $max = 0;
                             $Acquisition_data = $graph_year_data->InvestmentActivityData->$year_data->TotalAcquisitionsPrice;
@@ -34,11 +34,11 @@
                         </div>
                     <?php }?>
                 </div>
-                <div class="b" style="grid-template-columns:repeat(<?php echo( $grid_year_difference + 1) ?>,1fr);">
+                <div class="negative_part_of_graph" style="grid-template-columns:repeat(<?php echo( $grid_year_difference + 1) ?>,1fr);">
                     <?php
 
                     for ($year_data = $_GET['startYear']; $year_data <= $_GET['endYear']; $year_data++) {?>
-                        <div class="Graph_Data_chart2" id="<?php echo $year_data ?>">
+                        <div class="Graph_Data_negative_chart" id="<?php echo $year_data ?>">
                             <?php $Disposition_data = $graph_year_data->InvestmentActivityData->$year_data->TotalDispositionsPrice;
                             $Net_data = $graph_year_data->InvestmentActivityData->$year_data->NetValue;
                             //disposition bar
@@ -60,17 +60,16 @@
         </div>
     </div>
 
-    <div id="length2">
-
+    <div id="vol_bar">
         <?php $vol_counter=30;?>
 
-        <div class="divchart1" style="height:<?php echo number_format((28071422893.129/7)/100000000,1)?>px">vol ()</div>
+        <div style="height:<?php echo number_format((28071422893.129/7)/100000000,1)?>px">vol ()</div>
         <?php        for($loop_counter=0 ; $loop_counter<=6 ; $loop_counter++){?>
-        <div class="divchart1" style="height:<?php echo number_format((28071422893.129/7)/100000000,1)?>px"><?php  echo   $vol_counter .' B' ?> </div>
+        <div style="height:<?php echo number_format((28071422893.129/7)/100000000,1)?>px"><?php  echo   $vol_counter .' B' ?> </div>
         <?php $vol_counter -=5;
         }?>
         <?php $vol_counter=5;
         for($loop_counter=0 ; $loop_counter<=4; $loop_counter++){?>
-        <div class="divchart1" style="height:<?php echo number_format((28071422893.129/7)/100000000,1)?>px"><?php  echo   $vol_counter .' B' ?> </div>
+        <div style="height:<?php echo number_format((28071422893.129/7)/100000000,1)?>px"><?php  echo   $vol_counter .' B' ?> </div>
         <?php $vol_counter +=5; }?>
     </div>
